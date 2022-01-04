17dede035e56ee0549a741fc321b552ea869e8da
NKG丶MadLife <1778139321@qq.com>
2022-01-05 00:02:23 +0800

    fix：<玩家输入与属性同步> 补上广播

--- Unity/Assets/Hotfix/NKGMOBA/Battle/LockStepStateFrameSync/Handler/LSF_SyncAttributeHandler.cs
+++ Unity/Assets/Hotfix/NKGMOBA/Battle/LockStepStateFrameSync/Handler/LSF_SyncAttributeHandler.cs
@@ -9,6 +9,12 @@
             {
                 numericComponent[attributeToSync.Key] = attributeToSync.Value;
             }
+            
+#if SERVER
+            // 对于客户端发来的每一条指令，都要进行一次广播，因为多人模式需要进行同步，
+            LSF_Component lsfComponent = unit.BelongToRoom.GetComponent<LSF_Component>();
+            lsfComponent.AddCmdToSendQueue(cmd);
+#endif
 
             await ETTask.CompletedTask;
         }
